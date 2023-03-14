d332a50e4c8b86a581cb05cf8eda9397a2ddb1f0
1islar68 <1islar68@solent.ac.uk>
2023-03-14 10:49:28 +0000

    Week 4 Seminar

---
+++ basics/functions/function_with_nesting.py
@@ -0,0 +1,8 @@
+def identify():
+    response = input("What lies before us?\n")
+    if response == "a large boulder":
+        print("\nIt's time to run!")
+    else:
+        print("We will be fine.")
+
+identify()
---
+++ basics/functions/testing.py
@@ -0,0 +1,12 @@
+x = "Yeti"
+
+if x == "yeti":
+    print("Apple")
+else:
+    print("Orange")
+for i in range(6):
+    print("Pineapple")
+if x == "Pineapple":
+    print("Coconut")
+else:
+    print("Strawberries")
